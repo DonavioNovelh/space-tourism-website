ba34d6b789725cf725cc7f42c93c1136bffc373d
DonavioNovelh <donavioev@gmail.com>
2025-02-14 17:02:03 +0200

    m

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import Titulo from './components/Titulo'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +8,7 @@ function App() {
   return (
     <>
       <h1 className=''>Sua csa</h1>
+      <Titulo />
     </>
   )
 }
---
+++ src/components/Titulo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Titulo = () => {
+  return (
+    <div>
+      <h1>Hawenanananan</h1>
+      <img src="../../" alt="" />
+    </div>
+  );
+}
+
+export default Titulo
