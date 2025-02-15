06d8fffab9b84f29b617e0ddeda77364abb4e5df
DonavioNovelh <donavioev@gmail.com>
2025-02-15 19:58:36 +0200

    kk

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,21 +3,21 @@ import Logo from "../assets/icon/logo.svg"
 
 const Header = () => {
   return (
-    <div className="font-condenseda font-thin uppercase relative flex flex-row items-center ">
+    <div className="font-condenseda tracking-wide font-normal uppercase relative flex flex-row items-center ">
       <img className="" src={Logo} alt="" />
       <hr className="bg-white text-zinc-950 flex-1 mx-4 h-px " />
-      <div className="flex  w-2xl pt-4 pb-4 p-6 backdrop-blur-sm justify-start pl-9 gap-8">
-        <a className="text-white flex gap-7" href="">
+      <div className="flex  w-3xl pt-4 pb-4 p-6  backdrop-blur-3xl justify-start pl-14 gap-10 text-xs">
+        <a className="text-white flex gap-3" href="">
           <span>00</span> <span>Home</span>
         </a>
-        <a className="text-white flex gap-7" href="">
+        <a className="text-white flex gap-3" href="">
           <span>01</span> <span>Destination</span>
         </a>
-        <a className="text-white flex gap-7" href="">
+        <a className="text-white flex gap-3" href="">
           <span>02</span> <span>Crew</span>
         </a>
-        <a className="text-white flex gap-7" href="">
-          <span>03</span> <span>Tecnology</span>
+        <a className="text-white flex gap-3" href="">
+          <span>03</span> <span>Tecnhology</span>
         </a>
       </div>
     </div>
